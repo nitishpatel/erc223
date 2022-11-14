cffbe38e36a8315ad726393ab1ce1df0159406a2
nitish patel <nitishp74@gmail.com>
2022-11-14 14:05:41 +0530

    :rotating_light: Lint Fixes

--- contracts/ERC223Mintable.sol
+++ contracts/ERC223Mintable.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
@@ -7,18 +6,17 @@ import "./ERC223.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC223Mintable is ERC223Token, Ownable {
+  constructor(
+    string memory new_name,
+    string memory new_symbol,
+    uint8 new_decimals
+  ) ERC223Token(new_name, new_symbol, new_decimals) {}
 
-    constructor(
-        string memory new_name,
-        string memory new_symbol,
-        uint8 new_decimals
-    ) ERC223Token(new_name, new_symbol, new_decimals) {}
+  function mint(address to, uint256 value) public onlyOwner {
+    _mint(to, value);
+  }
 
-    function mint(address to, uint256 value) public  onlyOwner {
-        _mint(to, value);
-    }
-
-    function burn(address from, uint256 value) public onlyOwner {
-        _burn(from, value);
-    }
+  function burn(address from, uint256 value) public onlyOwner {
+    _burn(from, value);
+  }
 }
