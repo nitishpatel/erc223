6f8820c45d3dcf78bc953be75bb7ea367144ce2e
nitish patel <nitishp74@gmail.com>
2022-11-14 12:58:25 +0530

    :rocket: Deployment Code Added

---
+++ deploy/deploy.js
@@ -0,0 +1,21 @@
+const deployOptions = {
+    log: true,
+    gasPrice: ethers.utils.parseUnits("1", "gwei"),
+  };
+  
+  module.exports = async ({ getNamedAccounts, deployments, network }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    await deploy("ERC223Token", {
+      from: deployer,
+      args: [
+        "Test Token",
+        "TST",
+        18
+      ],
+      ...deployOptions,
+    });
+  };
+  
+  module.exports.tags = ["CustodianContract"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: 0,
+  }
 };
